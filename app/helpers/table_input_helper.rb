97bbb431325dbe0267e561e29eea302229229df7
Tim Harrison <heedspin@gmail.com>
2011-04-03 17:12:29 -0700

    Tidy up tables.

--- app/helpers/table_input_helper.rb
+++ app/helpers/table_input_helper.rb
@@ -1,5 +1,5 @@
 module TableInputHelper
-  def table_input(object, field, row, column)
-    text_field_tag "#{object}[#{field}][][#{row}][][#{column}]", nil, :size => 15
+  def table_input(object, field, row, column, size=15)
+    text_field_tag "#{object}[#{field}][][#{column}]", nil, :size => size
   end
 end
