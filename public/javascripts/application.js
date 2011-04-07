413768f6b5883b2933f97be27909685bf403e453
Tim Harrison <heedspin@gmail.com>
2011-04-06 21:53:17 -0700

    add finishing touches

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,10 +2,16 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function(){
-	jQuery(function() {
-	    jQuery('.wymeditor').wymeditor({
-	        skin: 'timpact',
-					stylesheet: '/stylesheets/wymeditor.css'
-	    });
+  $('.wymeditor').wymeditor({
+      skin: 'timpact',
+		stylesheet: '/stylesheets/wymeditor.css'
+  });
+
+
+	$(".toggle_button").bind('click', function(event) {
+	  event.preventDefault();
+	  $(this).closest("div.openable").toggleClass("open");
 	});
+
 });
+
